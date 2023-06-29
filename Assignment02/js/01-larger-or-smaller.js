c1542abdd3c4d1d5bf8bf60dccfbaefc74fffafb
mathyogageek <riewilsterman@mac.com>
2023-06-28 19:25:52 -0700

    Add files via upload

---
+++ Assignment02/js/01-larger-or-smaller.js
@@ -0,0 +1,26 @@
+// Call the function after the page has finished loading
+// automatically after the page loads
+window.onload = compareIntegers;
+
+// Wrap the code inside a function
+function compareIntegers() {
+  // Prompt for two integers
+  var x = parseInt(prompt("Enter the first integer:"));
+  var y = parseInt(prompt("Enter the second integer:"));
+
+  // Compare the two numbers
+  var result;
+  if (x > y) {
+    result = x;
+  } else if (y > x) {
+    result = y;
+  } else {
+    result = "The two numbers are equal.";
+  }
+
+  // Display the result in the browser window
+  document.body.innerHTML = "<h1>" + result + "</h1>";
+
+  
+}
+
